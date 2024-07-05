a401c6cf2209aeff86414a69d3b165d1cbfbd6b4
WilhelmYakunin <v.jakunin@inbox.ru>
2024-07-05 12:20:27 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/templator/dist'
+  // base: 'https://wilhelmyakunin.github.io/templator/'
 })
