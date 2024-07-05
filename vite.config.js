293957da0147b86e16fca216068f3d257504231f
WilhelmYakunin <v.jakunin@inbox.ru>
2024-07-05 12:55:18 +0300

    dep

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/templator/'
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/templator/' : '/'
 })
